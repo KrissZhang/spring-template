8f3b035c05767de48fd7dedd284d852b6b98f84f
ex_zhangpeng3@exiot.cmcc <Zp!18716331455>
2022-08-04 16:04:46 +0800

    系统字典

--- spring-common/src/main/java/com/self/common/constants/CacheConstants.java
+++ spring-common/src/main/java/com/self/common/constants/CacheConstants.java
@@ -7,6 +7,11 @@ package com.self.common.constants;
 public final class CacheConstants {
 
     /**
+     * 字典 key
+     */
+    public static final String SYS_DICT_KEY = "sys:dict:";
+
+    /**
      * 限流 redis key
      */
     public static final String RATE_LIMIT_KEY = "rate_limit:";
---
+++ spring-system/src/main/java/com/self/system/service/SysDictService.java
@@ -0,0 +1,46 @@
+package com.self.system.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.google.common.collect.Lists;
+import com.self.common.constants.CacheConstants;
+import com.self.common.utils.RedisUtils;
+import com.self.dao.entity.SysDictData;
+import com.self.dao.service.SysDictDataService;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+@Service
+public class SysDictService {
+
+    @Autowired
+    private RedisUtils redisUtils;
+
+    @Autowired
+    private SysDictDataService sysDictDataService;
+
+    public List<SysDictData> getDictList(String dictTypeCode){
+        if(StringUtils.isBlank(dictTypeCode)){
+            return Lists.newArrayListWithCapacity(0);
+        }
+
+        List<SysDictData> dictList = redisUtils.get(CacheConstants.SYS_DICT_KEY + dictTypeCode);
+
+        if(!CollectionUtils.isEmpty(dictList)){
+            return dictList;
+        }
+
+        LambdaQueryWrapper<SysDictData> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(SysDictData::getStatus, NumberUtils.BYTE_ONE).eq(SysDictData::getDictTypeCode, dictTypeCode);
+        dictList = sysDictDataService.list(wrapper);
+
+        redisUtils.set(CacheConstants.SYS_DICT_KEY + dictTypeCode, dictList);
+
+        return dictList;
+    }
+
+}
