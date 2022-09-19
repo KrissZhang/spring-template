82e6bbeeaa1d9baf1bcece431970d3a3b13c3440
ex_zhangpeng3@exiot.cmcc <Zp!18716331455>
2022-09-19 11:48:27 +0800

    druid管理页面配置化

--- spring-common/src/main/java/com/self/common/constants/CfgConstants.java
+++ spring-common/src/main/java/com/self/common/constants/CfgConstants.java
@@ -3,21 +3,24 @@ package com.self.common.constants;
 /**
  * 系统配置
  */
-public interface CfgConstants {
+public final class CfgConstants {
+
+    private CfgConstants() {
+    }
 
     /**
      * druid 登陆用户名
      */
-    String DRUID_LOGIN_USERNAME = "${spring.druid.login-username: admin}";
+    public static final String DRUID_LOGIN_USERNAME = "${spring.druid.login-username: admin}";
 
     /**
      * druid 登陆密码
      */
-    String DRUID_LOGIN_PASSWORD = "${spring.druid.login-password: 123}";
+    public static final String DRUID_LOGIN_PASSWORD = "${spring.druid.login-password: 123}";
 
     /**
      * druid 允许访问地址
      */
-    String DRUID_LOGIN_ALLOW = "${spring.druid.allow: 127.0.0.1}";
+    public static final String DRUID_LOGIN_ALLOW = "${spring.druid.allow: 127.0.0.1}";
 
 }
