d6d0f586c17e0096b3417b543399d29cbfc0ee90
ex_zhangpeng3@exiot.cmcc <Zp!18716331455>
2022-10-19 16:11:14 +0800

    数据库规范调整

--- sql/spring-template.sql
+++ sql/spring-template.sql
@@ -4,9 +4,8 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 -- Table structure for oper_log
 -- ----------------------------
-DROP TABLE IF EXISTS `oper_log`;
 CREATE TABLE `oper_log`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
   `terminal_type` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '终端类别',
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '标题',
   `business_type` tinyint(0) NULL DEFAULT NULL COMMENT '功能类型',
@@ -20,54 +19,48 @@ CREATE TABLE `oper_log`  (
   `resp_result` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '返回参数',
   `status` tinyint(0) NOT NULL COMMENT '操作状态',
   `error_msg` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '错误信息',
-  `operate_time` datetime(0) NOT NULL COMMENT '操作时间',
+  `operate_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '操作时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 31 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '操作日志信息表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of oper_log
--- ----------------------------
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '操作日志信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_dict_data
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_dict_data`;
 CREATE TABLE `sys_dict_data`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
   `dict_type_code` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '字典类型编码',
   `dict_label` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '字典标签',
   `dict_value` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '字典值',
   `dict_sort` int(0) NULL DEFAULT NULL COMMENT '字典排序',
-  `status` tinyint(0) NOT NULL COMMENT '状态，1-启用，0-停用',
+  `status` tinyint(0) NOT NULL COMMENT '状态，0-启用，1-停用',
   `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `create_user` int(0) NULL DEFAULT NULL COMMENT '创建人id',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_user` int(0) NULL DEFAULT NULL COMMENT '修改人id',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
+  `create_user` bigint(0) UNSIGNED NULL DEFAULT NULL COMMENT '创建人',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_user` bigint(0) UNSIGNED NULL DEFAULT NULL COMMENT '修改人',
+  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `normal_dict_type_code`(`dict_type_code`) USING BTREE COMMENT '字典类型编码索引'
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '字典数据表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '字典数据表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_dict_data
 -- ----------------------------
-INSERT INTO `sys_dict_data` VALUES (1, 'gender', '男', '1', 1, 1, NULL, 1, '2022-08-04 15:36:29', 1, '2022-08-04 15:37:05');
-INSERT INTO `sys_dict_data` VALUES (2, 'gender', '女', '2', 2, 1, NULL, 1, '2022-08-04 15:37:34', 1, '2022-08-04 15:37:38');
+INSERT INTO `sys_dict_data` VALUES (1, 'gender', '男', '1', 1, 0, NULL, 1, '2022-08-04 15:36:29', 1, '2022-08-04 15:37:05');
+INSERT INTO `sys_dict_data` VALUES (2, 'gender', '女', '2', 2, 0, NULL, 1, '2022-08-04 15:37:34', 1, '2022-08-04 15:37:38');
 
 -- ----------------------------
 -- Table structure for sys_dict_type
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_dict_type`;
 CREATE TABLE `sys_dict_type`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
   `dict_type_code` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '字典类型编码',
   `dict_type_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '字典类型名称',
-  `status` tinyint(0) NOT NULL COMMENT '状态，1-启用，0-停用',
+  `status` tinyint(0) NOT NULL COMMENT '状态，0-启用，1-停用',
   `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `create_user` int(0) NULL DEFAULT NULL COMMENT '创建人id',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_user` int(0) NULL DEFAULT NULL COMMENT '修改人id',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
+  `create_user` bigint(0) UNSIGNED NULL DEFAULT NULL COMMENT '创建人',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_user` bigint(0) UNSIGNED NULL DEFAULT NULL COMMENT '修改人',
+  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `unique_dict_type_code`(`dict_type_code`) USING BTREE COMMENT '字典类型编码唯一'
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '字典类型表' ROW_FORMAT = Dynamic;
@@ -75,43 +68,45 @@ CREATE TABLE `sys_dict_type`  (
 -- ----------------------------
 -- Records of sys_dict_type
 -- ----------------------------
-INSERT INTO `sys_dict_type` VALUES (1, 'gender', '性别', 1, NULL, 1, '2022-08-04 15:38:13', 1, '2022-08-04 15:38:16');
+INSERT INTO `sys_dict_type` VALUES (1, 'gender', '性别', 0, NULL, 1, '2022-08-04 15:38:13', 1, '2022-08-04 15:38:16');
 
 -- ----------------------------
 -- Table structure for test
 -- ----------------------------
-DROP TABLE IF EXISTS `test`;
 CREATE TABLE `test`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '名称',
   `value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '值',
-  `enable` tinyint(0) NOT NULL COMMENT '是否启用',
+  `is_deleted` tinyint(1) UNSIGNED ZEROFILL NOT NULL DEFAULT 0 COMMENT '是否逻辑删除，0-未删除，1-已删除',
+  `create_user` bigint(0) UNSIGNED NULL DEFAULT NULL COMMENT '创建人',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_user` bigint(0) UNSIGNED NULL DEFAULT NULL COMMENT '修改人',
+  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '测试信息表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of test
--- ----------------------------
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '测试信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
   `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
   `real_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户真实名称',
   `phone_num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '手机号码',
-  `enable` tinyint(0) NOT NULL COMMENT '是否启用',
+  `is_deleted` tinyint(1) UNSIGNED ZEROFILL NOT NULL DEFAULT 0 COMMENT '是否逻辑删除，0-未删除，1-已删除',
+  `create_user` bigint(0) UNSIGNED NULL DEFAULT NULL COMMENT '创建人',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_user` bigint(0) UNSIGNED NULL DEFAULT NULL COMMENT '修改人',
+  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `unique_user_name`(`user_name`) USING BTREE COMMENT '用户名唯一',
   UNIQUE INDEX `unique_phone_num`(`phone_num`) USING BTREE COMMENT '手机号码唯一'
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户信息表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'user1', 'e10adc3949ba59abbe56e057f20f883e', '用户1', '13983387265', 1);
+INSERT INTO `user` VALUES (1, 'user1', 'e10adc3949ba59abbe56e057f20f883e', '用户1', '13983387265', 0, 1, '2022-10-19 15:45:21', 1, '2022-10-19 15:59:49');
 
 SET FOREIGN_KEY_CHECKS = 1;
